d889270af75ce8605782de916bb2b3b9c6215733
teodorkasap <erol.erten@gmail.com>
2019-01-12 13:01:19 +0300

    Initial Project Add

---
+++ src/main/java/com/erolerten/eepetclinic/EePetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.erolerten.eepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EePetClinicApplication.class, args);
+    }
+
+}
+
