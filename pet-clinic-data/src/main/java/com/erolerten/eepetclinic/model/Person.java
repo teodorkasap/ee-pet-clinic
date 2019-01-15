384af35e94103010a404e210829d222f1b9e7160
teodorkasap <erol.erten@gmail.com>
2019-01-15 23:58:24 +0300

    committing interfaces for services. closes #5 committing BaseEntity Class. closes #10

--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.erolerten.eepetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.erolerten.eepetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
