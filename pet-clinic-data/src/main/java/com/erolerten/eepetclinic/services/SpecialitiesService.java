36c81f6428c413d0ae4e2e2c2fb0ebb5c38643cd
teodorkasap <erol.erten@gmail.com>
2019-01-22 23:21:44 +0300

    Added Specialties Map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/services/SpecialitiesService.java
@@ -0,0 +1,8 @@
+package com.erolerten.eepetclinic.services;
+
+import com.erolerten.eepetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+
+
+}
