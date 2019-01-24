e519bb15cc8fcb543a1f73c9eb82a9c20b850063
teodorkasap <erol.erten@gmail.com>
2019-01-24 19:50:39 +0300

    updated visit with JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
@@ -1,10 +1,10 @@
 package com.erolerten.eepetclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity(name = "pets")
 public class Pet extends BaseEntity{
 
@@ -21,6 +21,9 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits =new HashSet<>();
 
     public String getName() {
         return name;
@@ -52,5 +55,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
