99ee606a8e0aee092adc2fa3f8b5d184c787d195
teodorkasap <erol.erten@gmail.com>
2019-01-25 18:53:43 +0300

    added spring data jpa service for owners. closes #40

--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/repositories/OwnerRepository.java
@@ -4,6 +4,6 @@ import com.erolerten.eepetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName (String lastName);
 
 }
