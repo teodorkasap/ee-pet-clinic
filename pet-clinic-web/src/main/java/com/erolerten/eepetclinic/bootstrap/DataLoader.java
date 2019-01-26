a31291babec99d8171cd44d9b47887b2a76aadcc
teodorkasap <erol.erten@gmail.com>
2019-01-26 16:00:49 +0300

    Added project lombok. Closes #51

--- pet-clinic-web/src/main/java/com/erolerten/eepetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/erolerten/eepetclinic/bootstrap/DataLoader.java
@@ -70,6 +70,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("istanbul");
         owner1.setTelephone("1234567899");
 
+        //Owner.builder().address("taksim").city("istanbul").firstName("erol").lastName("erten");
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
