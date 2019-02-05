5043220f10610fdcb145fc867310fa4440d35b16
teodorkasap <erol.erten@gmail.com>
2019-02-05 23:53:17 +0300

    Implemented find owner. closes #54. Listing all owners in the search drop down does not work, needs fixing.

--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.erolerten.eepetclinic.repositories;
 import com.erolerten.eepetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName (String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/services/OwnerService.java
@@ -2,10 +2,12 @@ package com.erolerten.eepetclinic.services;
 
 import com.erolerten.eepetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long > {
 
     Owner findByLastName(String lastName);
 
-
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
