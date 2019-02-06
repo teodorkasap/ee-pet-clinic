073e6e3533120c48c61467943da7bd8a5fdb91d3
teodorkasap <erol.erten@gmail.com>
2019-02-06 22:38:24 +0300

    Add ability to add or update a pet closes #56

--- pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/erolerten/eepetclinic/model/Pet.java
@@ -11,9 +11,19 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity(name = "pets")
 public class Pet extends BaseEntity{
+
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+        this.visits = visits;
+    }
 
     @Column(name = "name")
     private String name;
